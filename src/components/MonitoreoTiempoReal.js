e1b45b57bf1f0862b2d8faef61d96d6ce59efe14
Tono9087 <antonytorres1300@gmail.com>
2026-03-10 20:43:12 -0600

    useRef error

--- src/components/MonitoreoTiempoReal.js
+++ src/components/MonitoreoTiempoReal.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import YouTube from 'react-youtube';
 import { obtenerConfiguracionPeluche, obtenerLecturasRecientes } from '../pelucheUtils';
 import { gestorAudio, sonidosDisponibles, videosYouTube } from '../soundManager';
